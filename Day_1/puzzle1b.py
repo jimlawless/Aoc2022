99e2f258a815bd3cb8564677ecbb5dc5b5ccd224
Jim Lawless <lawless.jim@gmail.com>
2022-12-01 12:27:09 -0600

    Streamlined the code.

--- Day_1/puzzle1b.py
+++ Day_1/puzzle1b.py
@@ -3,18 +3,14 @@
 
 if __name__ == "__main__":
     infile = open("input1.txt","r")
-    elf_count = 1
     total=0
     elves=[]
-    elf_count = 0
     for line in infile:
         if(len(line)>1):
             total = total + int(line)
         else:
             elves.insert(0,total)
-            elf_count = elf_count + 1
             total = 0
     infile.close()
-    elves.sort()
-    last_three = elves[elf_count-1]+elves[elf_count-2]+elves[elf_count-3]
-    print(last_three)
+    elves.sort(reverse=True)
+    print(elves[0]+elves[1]+elves[2])
