0bee70175a236a975a134b449760a35722631e27
Jim Lawless <lawless.jim@gmail.com>
2022-12-02 06:08:45 -0600

    Updated code with table
    All of the situations can be precomputed with a table lookup.

--- Day_2/puzzle2.py
+++ Day_2/puzzle2.py
@@ -1,25 +1,13 @@
 # Jim Lawless
 # License: https://github.com/jimlawless/AoC2022/LICENSE
 
-m={ "A" : 1, "B" : 2, "C" : 3, "X" : 1, "Y" : 2, "Z" : 3}
+tab={ "AX":4,"AY":8,"AZ":3,"BX":1,"BY":5,"BZ":9,"CX":7,"CY":2,"CZ":6 } 
 
 if __name__ == "__main__":
     total = 0
     infile = open("input2.txt","r")
     for line in infile:
-        pair=line.split()
-        if (pair[0]=="A")and(pair[1]=="X"):
-            total=total+3
-        elif (pair[0]=="A")and(pair[1]=="Y"):
-            total=total+6
-        elif (pair[0]=="B")and(pair[1]=="Y"):
-            total=total+3
-        elif (pair[0]=="B")and(pair[1]=="Z"):
-            total=total+6
-        elif (pair[0]=="C")and(pair[1]=="X"):
-            total=total+6
-        elif (pair[0]=="C")and(pair[1]=="Z"):
-            total=total+3
-        total=total+m[pair[1]]
+        them,me=line.split()
+        total+=tab[them+me]
     infile.close()
     print(total)
--- Day_2/puzzle2b.py
+++ Day_2/puzzle2b.py
@@ -1,36 +1,13 @@
 # Jim Lawless
 # License: https://github.com/jimlawless/AoC2022/LICENSE
 
-m={ "A" : 1, "B" : 2, "C" : 3, "X" : 1, "Y" : 2, "Z" : 3}
+tab={ "AX":3,"AY":4,"AZ":8,"BX":1,"BY":5,"BZ":9,"CX":2,"CY":6,"CZ":7 } 
 
 if __name__ == "__main__":
     total = 0
     infile = open("input2.txt","r")
     for line in infile:
-        pair=line.split()
-        if (pair[0]=="A")and(pair[1]=="X"):
-            total=total+m["Z"]
-        elif (pair[0]=="A")and(pair[1]=="Y"):
-            total=total+3
-            total=total+m["X"]
-        elif (pair[0]=="A")and(pair[1]=="Z"):
-            total=total+6
-            total=total+m["Y"]
-        elif (pair[0]=="B")and(pair[1]=="X"):
-            total=total+m["X"]
-        elif (pair[0]=="B")and(pair[1]=="Y"):
-            total=total+3
-            total=total+m["Y"]
-        elif (pair[0]=="B")and(pair[1]=="Z"):
-            total=total+6
-            total=total+m["Z"]       
-        elif (pair[0]=="C")and(pair[1]=="X"):
-            total=total+m["Y"]
-        elif (pair[0]=="C")and(pair[1]=="Y"):
-            total=total+3
-            total=total+m["Z"]
-        elif (pair[0]=="C")and(pair[1]=="Z"):
-            total=total+6
-            total=total+m["X"]
+        them,me=line.split()
+        total+=tab[them+me]
     infile.close()
     print(total)
