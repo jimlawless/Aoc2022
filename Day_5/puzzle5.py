2ef7189ac0be91a13434dfeb94fddc92481cd4a4
Jim Lawless <lawless.jim@gmail.com>
2022-12-04 23:51:37 -0600

    Add files via upload

---
+++ Day_5/puzzle5.py
@@ -0,0 +1,33 @@
+# Jim Lawless
+# License: https://github.com/jimlawless/AoC2022/LICENSE
+
+if __name__ == "__main__":
+    count=0
+    tab=[]
+    for i in range(0,10):
+        tab.insert(i,[])
+    infile = open("input5.txt","r")
+    lines = infile.readlines()
+    infile.close()
+    while count < len(lines):
+        if(len(lines[count])<2):
+            break
+        for i in range(1,len(lines[count]),4):
+            if lines[count][i].isalpha():      
+                tab[int(i/4)].append(lines[count][i])
+        count+=1
+    count+=1
+    while count < len(lines):
+        words = lines[count].split()
+        num = int(words[1])
+        frm = int(words[3])-1
+        to = int(words[5])-1
+        for i in range(0,num):
+            tmp=tab[frm].pop(0)
+            tab[to].insert(0,tmp)
+        count+=1
+    for i in range(0,10):
+        if(len(tab[i])>0):
+            tmp=tab[i].pop(0)
+            print(tmp,end="")
+        
