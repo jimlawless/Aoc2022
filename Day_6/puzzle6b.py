61b06c6a38575a68afc81ee7702224d93f92ccd6
Jim Lawless <lawless.jim@gmail.com>
2022-12-05 23:28:18 -0600

    Add files via upload

---
+++ Day_6/puzzle6b.py
@@ -0,0 +1,18 @@
+# Jim Lawless
+# License: https://github.com/jimlawless/AoC2022/LICENSE
+
+if __name__ == "__main__":
+    infile = open("input6.txt","r")
+    for line in infile:
+        for i in range(13,len(line)):
+            tmp="".join(sorted(line[i-13:i+1]))
+            found=False
+            for j in range(1,len(tmp)):
+                if tmp[j]==tmp[j-1]:
+                    found=True
+            if found:
+                continue            
+            print(i+1)
+            break                     
+    infile.close()
+ 
